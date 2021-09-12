d89bde7197c9d047988705142540c2a9d44160fd
leeheechan <coolzz7@naver.com>
2021-09-12 14:25:11 +0900

    feat: basic auth security 구현

---
+++ src/main/java/kr/ac/hs/selab/auth/application/AuthService.java
@@ -0,0 +1,26 @@
+package kr.ac.hs.selab.auth.application;
+
+import kr.ac.hs.selab.member.domain.vo.Email;
+import kr.ac.hs.selab.member.infrastructure.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(new Email(username))
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.");
+                })
+                .toAuthPrincipal();
+    }
+}
